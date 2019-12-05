9de30f39a8fd8770a61138ae0a9d5e52f4fcf3a0
Scott Bauer <sbauer322@gmail.com>
2019-12-04 22:09:33 -0500

    Add day 3 part 2

--- aoc-2019/lib/aoc/day3/crossed_wires.ex
+++ aoc-2019/lib/aoc/day3/crossed_wires.ex
@@ -1,6 +1,9 @@
 defmodule AOC.Day3.CrossedWires do
-
   @moduledoc false
+
+  @type point :: {integer, integer}
+  @type segment :: [point, ...]
+  @type wire :: list(point)
 
   def part1() do
     [w1, w2] =
@@ -11,6 +14,11 @@ defmodule AOC.Day3.CrossedWires do
   end
 
   def part2() do
+    [w1, w2] =
+      stream_input("./resources/day3_part1_input.txt")
+      |> process_input()
+
+    compute_part2(w1, w2)
   end
 
   def stream_input(path) do
@@ -30,12 +38,7 @@ defmodule AOC.Day3.CrossedWires do
     end)
   end
 
-  def compute_part1(w1, w2) do
-    find_intersections(w1, w2)
-    |> Enum.min_by(&manhattan_distance(&1, {0, 0}))
-    |> manhattan_distance({0, 0})
-  end
-
+  @spec process_wire_input(list(String.t)) :: list({atom, integer})
   def process_wire_input(wire) do
     wire
     |> Enum.map(fn vector ->
@@ -46,6 +49,7 @@ defmodule AOC.Day3.CrossedWires do
     end)
   end
 
+  @spec points(list({atom, integer})) :: list(point)
   def points(path) do
     path
     |> Enum.reduce([{0, 0}], fn {direction, quantity}, acc ->
@@ -64,11 +68,44 @@ defmodule AOC.Day3.CrossedWires do
     |> Enum.reverse()
   end
 
+  @spec compute_part1(wire, wire) :: non_neg_integer
+  def compute_part1(w1, w2) do
+    w1_segments = Enum.chunk_every(w1, 2, 1, :discard)
+    w2_segments = Enum.chunk_every(w2, 2, 1, :discard)
+
+    find_intersections(w1_segments, w2_segments)
+    |> Enum.min_by(&manhattan_distance(&1, {0, 0}))
+    |> manhattan_distance({0, 0})
+  end
+
+  @spec compute_part2(wire, wire) :: float
+  def compute_part2(w1, w2) do
+    w1_segments = Enum.chunk_every(w1, 2, 1, :discard)
+    w2_segments = Enum.chunk_every(w2, 2, 1, :discard)
+
+    intersections = find_intersections(w1_segments, w2_segments)
+
+    w1_sums =
+      Enum.map(intersections, fn intersection ->
+        sum_segments_until(w1_segments, intersection)
+      end)
+
+    w2_sums =
+      Enum.map(intersections, fn intersection ->
+        sum_segments_until(w2_segments, intersection)
+      end)
+
+    Enum.zip(w1_sums, w2_sums)
+    |> Enum.map(fn {a, b} -> a + b end)
+    |> Enum.min()
+  end
+
   @doc """
   Adapted from
   https://stackoverflow.com/a/24392281/3141194
   http://paulbourke.net/geometry/pointlineplane/javascript.txt
   """
+  @spec segments_intersect(segment, segment) :: point | :none
   def segments_intersect([start1, end1], [start2, end2]) do
     {a, b} = start1
     {c, d} = end1
@@ -88,23 +125,41 @@ defmodule AOC.Day3.CrossedWires do
     end
   end
 
-  def find_intersections(w1, w2) do
-    w1_segments = Enum.chunk_every(w1, 2, 1, :discard)
-    w2_segments = Enum.chunk_every(w2, 2, 1, :discard)
-
+  @spec find_intersections(list(segment), list(segment)) :: list(point)
+  def find_intersections(w1_segments, w2_segments) do
     Enum.reduce(w1_segments, [], fn segment1, acc ->
       intersections =
         Enum.reduce(w2_segments, [], fn segment2, acc ->
           intersections = segments_intersect(segment1, segment2)
 
-          if :none == intersections do
-            acc
-          else
+          if :none != intersections do
             [intersections | acc]
+          else
+            acc
           end
         end)
 
       intersections ++ acc
+    end)
+  end
+
+  @spec point_on_segment?(segment, point) :: boolean
+  def point_on_segment?(segment, point) do
+    [start, stop] = segment
+
+    manhattan_distance(start, stop) ==
+      manhattan_distance(start, point) + manhattan_distance(stop, point)
+  end
+
+  @spec sum_segments_until(list(segment), point) :: non_neg_integer
+  def sum_segments_until(segments, point) do
+    segments
+    |> Enum.reduce_while(0, fn [start, stop], acc ->
+      if point_on_segment?([start, stop], point) do
+        {:halt, acc + manhattan_distance(start, point)}
+      else
+        {:cont, acc + manhattan_distance(start, stop)}
+      end
     end)
   end
 
--- aoc-2019/test/day3/crossed_wires_test.exs
+++ aoc-2019/test/day3/crossed_wires_test.exs
@@ -25,10 +25,36 @@ defmodule AOC.Day3.CrossedWiresTest do
     assert expected == result
   end
 
-
   test "part 1" do
     expected = 2050
     result = CrossedWires.part1()
+
+    assert expected == result
+  end
+
+  test "part 2 example 1" do
+    raw_w1 = ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"]
+    raw_w2 = ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
+    expected = 610
+    [w1, w2] = CrossedWires.process_input([raw_w1, raw_w2])
+    result = CrossedWires.compute_part2(w1, w2)
+
+    assert expected == result
+  end
+
+  test "part 2 example 2" do
+    raw_w1 = ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"]
+    raw_w2 = ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]
+    expected = 410
+    [w1, w2] = CrossedWires.process_input([raw_w1, raw_w2])
+    result = CrossedWires.compute_part2(w1, w2)
+
+    assert expected == result
+  end
+
+  test "part 2" do
+    expected = 21_666
+    result = CrossedWires.part2()
 
     assert expected == result
   end
