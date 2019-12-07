a0318441388dd0e55bc2bf997e48a0d43e83ea8e
Scott Bauer <sbauer322@gmail.com>
2019-12-07 13:55:24 -0500

    Add day 5 part 2

--- aoc-2019/lib/aoc/day5/intcode.ex
+++ aoc-2019/lib/aoc/day5/intcode.ex
@@ -1,28 +1,42 @@
 defmodule AOC.Day5.Intcode do
   @moduledoc false
 
-  @type memory :: %{integer => integer}
+  @type memory :: %{
+                    integer => integer,
+                    pointer: integer,
+                    inputs: list(integer),
+                    outputs: list(integer)
+                  }
 
-  def part1(path) do
-    stream_input(path)
-    |> input_to_map()
+  def part1(path, user_inputs) do
+    stream_puzzle_input(path)
+    |> puzzle_input_to_map(user_inputs)
     |> compute
   end
 
-  def stream_input(path) do
+  def part2(path, user_inputs) do
+    stream_puzzle_input(path)
+    |> puzzle_input_to_map(user_inputs)
+    |> compute
+  end
+
+  def stream_puzzle_input(path) do
     File.read!(path)
     |> String.trim()
     |> String.split(",")
   end
 
-  @spec input_to_map(list(integer)) :: memory
-  def input_to_map(input) do
-    input
+  @spec puzzle_input_to_map(list(integer), list(integer)) :: memory
+  def puzzle_input_to_map(puzzle_input, user_input \\ []) do
+    puzzle_input
     |> Stream.with_index()
     |> Stream.map(fn {value, index} ->
       {index, String.to_integer(value)}
     end)
     |> Map.new()
+    |> (&(Map.put(&1, :pointer, 0))).()
+    |> (&(Map.put(&1, :inputs, user_input))).()
+    |> (&(Map.put(&1, :outputs, []))).()
   end
 
   @spec brute_force(memory, integer, integer, integer) :: {integer, integer} | :error
@@ -41,27 +55,27 @@ defmodule AOC.Day5.Intcode do
     end
   end
 
-  @spec compute(memory) :: integer
+  @spec compute(memory) :: list(integer)
   def compute(memory) do
     0..map_size(memory)
-    |> Enum.reduce_while({memory, 0}, fn _i, {memory, instruction_pointer} ->
-      with {parameter_modes, opcode} <- process_address(memory, instruction_pointer),
+    |> Enum.reduce_while(memory, fn _i, memory ->
+      with {parameter_modes, opcode} <- process_address(memory),
            {operation, num_params} <- instruction(memory, opcode),
            false <- is_atom(operation),
-           params <- read_params(memory, instruction_pointer, num_params),
+           params <- read_params(memory, num_params),
            params <- Enum.zip(params, parameter_modes),
-           result <- apply(operation, [memory | params]),
+           result <- apply(operation, [{memory, num_params} | params]),
            false <- is_atom(result) do
-        {:cont, {result, instruction_pointer + num_params}}
+        {:cont, result}
       else
         _ ->
-          IO.puts("Halting")
-          {:halt, read(memory, 0)}
+          {:halt, Enum.reverse(read_outputs(memory))}
       end
     end)
   end
 
-  def process_address(memory, address) do
+  def process_address(memory) do
+    address = read_instruction_pointer(memory)
     {parameter_modes, op} = read(memory, address)
                             |> Integer.digits()
                             |> Enum.split(-2)
@@ -71,12 +85,13 @@ defmodule AOC.Day5.Intcode do
     {parameter_modes, op}
   end
 
-  @spec read_params(memory, integer, integer) :: list(integer)
-  def read_params(memory, instruction_pointer, num_params) do
+  @spec read_params(memory, integer) :: list(integer)
+  def read_params(memory, num_params) do
+    address = read_instruction_pointer(memory)
     num_params = num_params - 2
 
     if num_params >= 0 do
-      Enum.map(0..num_params, fn i -> read(memory, i + instruction_pointer + 1) end)
+      Enum.map(0..num_params, fn i -> read(memory, i + address + 1) end)
     else
       []
     end
@@ -96,13 +111,53 @@ defmodule AOC.Day5.Intcode do
     Map.get(memory, address)
   end
 
+  @spec read_instruction_pointer(memory) :: integer
+  def read_instruction_pointer(memory) do
+    memory.pointer
+  end
+
+  @spec update_instruction_pointer(memory, integer) :: memory
+  def update_instruction_pointer(memory, value) do
+    %{memory | :pointer => value}
+  end
+
+  @spec increment_instruction_pointer(memory, integer) :: memory
+  def increment_instruction_pointer(memory, value) do
+    pointer = read_instruction_pointer(memory)
+    update_instruction_pointer(memory, pointer + value)
+  end
+
+  @spec pop_input(memory) :: {integer, memory}
+  def pop_input(memory) do
+    [head | tail] = memory.inputs
+    memory = %{memory | :inputs => tail}
+    {head, memory}
+  end
+
+  @spec append_input(memory, integer) :: memory
+  def append_input(memory, value) do
+    inputs = memory.inputs
+    %{memory | :inputs => inputs ++ [value]}
+  end
+
+  @spec read_outputs(memory) :: list(integer)
+  def read_outputs(memory) do
+    memory.outputs
+  end
+
+  @spec push_output(memory, integer) :: memory
+  def push_output(memory, value) do
+    outputs = memory.outputs
+    %{memory | :outputs => [value | outputs]}
+  end
+
   @spec update(memory, integer, integer) :: memory
   def update(memory, address, value) do
     %{memory | address => value}
   end
 
   @spec instruction(memory, integer) :: {(... -> memory) | :error, integer}
-  def instruction(memory, opcode) do
+  def instruction(_memory, opcode) do
     cond do
       opcode == 1 ->
         func = &add/4
@@ -124,6 +179,26 @@ defmodule AOC.Day5.Intcode do
         {:arity, num_params} = Function.info(func, :arity)
         {func, num_params}
 
+      opcode == 5 ->
+        func = &jump_if_true/3
+        {:arity, num_params} = Function.info(func, :arity)
+        {func, num_params}
+
+      opcode == 6 ->
+        func = &jump_if_false/3
+        {:arity, num_params} = Function.info(func, :arity)
+        {func, num_params}
+
+      opcode == 7 ->
+        func = &less_than/4
+        {:arity, num_params} = Function.info(func, :arity)
+        {func, num_params}
+
+      opcode == 8 ->
+        func = &equals/4
+        {:arity, num_params} = Function.info(func, :arity)
+        {func, num_params}
+
       opcode == 99 ->
         func = &terminate/1
         {:arity, num_params} = Function.info(func, :arity)
@@ -134,31 +209,79 @@ defmodule AOC.Day5.Intcode do
     end
   end
 
-  @spec add(memory, integer, integer, integer) :: memory
-  def add(memory, param1, param2, {param3, 0}) do
-    value = read(memory, param1) + read(memory, param2)
+  @spec add({memory, integer}, {integer, integer}, {integer, integer}, {integer, integer}) :: memory
+  def add({memory, num_params}, param_and_mode1, param_and_mode2, {param3, 0}) do
+    value = read(memory, param_and_mode1) + read(memory, param_and_mode2)
     update(memory, param3, value)
+    |> increment_instruction_pointer(num_params)
   end
 
-  @spec multiply(memory, integer, integer, integer) :: memory
-  def multiply(memory, param1, param2, {param3, 0}) do
-    value = read(memory, param1) * read(memory, param2)
+  @spec multiply({memory, integer}, {integer, integer}, {integer, integer}, {integer, integer}) :: memory
+  def multiply({memory, num_params}, param_and_mode1, param_and_mode2, {param3, 0}) do
+    value = read(memory, param_and_mode1) * read(memory, param_and_mode2)
     update(memory, param3, value)
+    |> increment_instruction_pointer(num_params)
   end
 
-  @spec input(memory, integer) :: memory
-  def input(memory, {param1, 0}) do
-    {value, _} = IO.gets("Awaiting input:")
-          |> Integer.parse()
+  @spec input({memory, integer}, {integer, integer}) :: memory
+  def input({memory, num_params}, {param1, 0}) do
+    {value, memory} = pop_input(memory)
     update(memory, param1, value)
+    |> increment_instruction_pointer(num_params)
   end
 
-  @spec output(memory, integer) :: memory
-  def output(memory, param1) do
+  @spec output({memory, integer}, {integer, integer}) :: memory
+  def output({memory, num_params}, param1) do
     read(memory, param1)
-    |> (&(IO.puts("Output: #{&1}"))).()
+  #    |> (&(IO.puts("Output: #{&1}"))).()
+    |> (&(push_output(memory, &1))).()
+    |> increment_instruction_pointer(num_params)
+  end
 
-    memory
+  def jump_if_true({memory, num_params}, param_and_mode1, param_and_mode2) do
+    v1 = read(memory, param_and_mode1)
+    v2 = read(memory, param_and_mode2)
+
+    if (v1 != 0) do
+      update_instruction_pointer(memory, v2)
+    else
+      increment_instruction_pointer(memory, num_params)
+    end
+  end
+
+  def jump_if_false({memory, num_params}, param_and_mode1, param_and_mode2) do
+    v1 = read(memory, param_and_mode1)
+    v2 = read(memory, param_and_mode2)
+
+    if (v1 == 0) do
+      update_instruction_pointer(memory, v2)
+    else
+      increment_instruction_pointer(memory, num_params)
+    end
+  end
+
+  def less_than({memory, num_params}, param_and_mode1, param_and_mode2, {param3, 0}) do
+    v1 = read(memory, param_and_mode1)
+    v2 = read(memory, param_and_mode2)
+
+    if (v1 < v2) do
+      update(memory, param3, 1)
+    else
+      update(memory, param3, 0)
+    end
+    |> increment_instruction_pointer(num_params)
+  end
+
+  def equals({memory, num_params}, param_and_mode1, param_and_mode2, {param3, 0}) do
+    v1 = read(memory, param_and_mode1)
+    v2 = read(memory, param_and_mode2)
+
+    if (v1 == v2) do
+      update(memory, param3, 1)
+    else
+      update(memory, param3, 0)
+    end
+    |> increment_instruction_pointer(num_params)
   end
 
   @spec terminate(memory) :: :terminate
--- aoc-2019/test/day5/intcode_test.exs
+++ aoc-2019/test/day5/intcode_test.exs
@@ -7,26 +7,172 @@ defmodule AOC.Day5.IntcodeTest do
 
   test "parameter mode" do
     input = ["1002","4","3","4","33"]
-    result =
-      Intcode.input_to_map(input)
+    outputs =
+      Intcode.puzzle_input_to_map(input)
       |> Intcode.compute()
 
-    assert 2 == result
+    assert [] == outputs
   end
 
   test "valid program with negative numbers" do
     input = ["1101","100","-1","4","0"]
-    Intcode.input_to_map(input)
+    outputs = Intcode.puzzle_input_to_map(input)
       |> Intcode.compute()
+
+    assert [] == outputs
   end
 
-#  test "part 1" do
-#    IO.puts("Enter '1' for the first input. Final output should be '12440243'")
-#    result = Intcode.part1("./resources/day5_part1_input.txt")
-#  end
+  test "If input is 8, output is 1. Otherwise 0." do
+    puzzle_input = ["3","9","8","9","10","9","4","9","99","-1","8"]
+
+    user_input = [8]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+
+    user_input = [1]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+
+    user_input = [10]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+  end
+
+  test "If input is < 8, output is 1. Otherwise 0." do
+    puzzle_input = ["3","9","7","9","10","9","4","9","99","-1","8"]
+
+    user_input = [8]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+
+    user_input = [-34]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+
+    user_input = [11]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+  end
+
+  test "Input is equal to 8; output 1 (if it is) or 0 (if it is not)" do
+    puzzle_input = ["3","3","1108","-1","8","3","4","3","99"]
+
+    user_input = [8]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+
+    user_input = [100]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+
+    user_input = [0]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+  end
+
+  test "Input is less than 8; output 1 (if it is) or 0 (if it is not)" do
+    puzzle_input = ["3","3","1107","-1","8","3","4","3","99"]
+
+    user_input = [8]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+    |> Intcode.compute()
+    assert [0] == outputs
+
+    user_input = [1]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+
+    user_input = [9]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+  end
+
+  test "(position mode) Output 0 if the input was zero, or output 1 if the input was non-zero." do
+    puzzle_input = ~w(3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9)
+
+    user_input = [0]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+
+    user_input = [1]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+
+    user_input = [99]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+  end
+
+  test "(immediate mode) Output 0 if the input was zero or 1 if the input was non-zero." do
+    puzzle_input = ~w(3 3 1105 -1 9 1101 0 0 12 4 12 99 1)
+
+    user_input = [0]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [0] == outputs
+
+    user_input = [1]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+
+    user_input = [-1]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1] == outputs
+  end
+
+  test "large jump test" do
+    # output 999 if the input value is below 8,
+    # output 1000 if the input value is equal to 8,
+    # or output 1001 if the input value is greater than 8.
+    puzzle_input = ~w(3 21 1008 21 8 20 1005 20 22 107 8 21 20 1006 20 31
+                1106 0 36 98 0 0 1002 21 125 20 4 20 1105 1 46 104
+                999 1105 1 46 1101 1000 1 20 4 20 1105 1 46 98 99)
+
+    user_input = [3]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [999] == outputs
+
+    user_input = [8]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1_000] == outputs
+
+    user_input = [79]
+    outputs = Intcode.puzzle_input_to_map(puzzle_input, user_input)
+              |> Intcode.compute()
+    assert [1_001] == outputs
+  end
+
+  test "part 1" do
+    user_input = [1]
+    last_output = Intcode.part1("./resources/day5_part1_input.txt", user_input)
+                   |> Enum.reverse()
+                   |> hd()
+    assert 12_440_243 == last_output
+  end
 
   test "part 2" do
-    IO.puts("Enter '1' for the first input. Final output should be '12440243'")
-    result = Intcode.part1("./resources/day5_part1_input.txt")
+    user_input = [5]
+    last_output = Intcode.part2("./resources/day5_part1_input.txt", user_input)
+                  |> Enum.reverse()
+                  |> hd()
+    assert 15_486_302 == last_output
   end
 end
